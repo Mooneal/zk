bb5a40e11ec9c413589ed3eaa5a3670189ae3174
Mooneal <horse-chestnut@foxmail.com>
2017-06-06 20:45:10 +0800

    worker2 add student

---
+++ Student.java
@@ -0,0 +1,10 @@
+/**
+ * Created by horse on 2017/5/1.
+ */
+public class Student implements Person{
+
+	@Override
+	public void  say(String string) {
+		System.out.println("lalalalla"+string);
+	}
+}
